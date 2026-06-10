5cb4dbce46b6f6f7c11af0a4ec56af0daf2a73b7
Lucas Delaunoit <lucasdelaunoit@gmail.com>
2026-06-10 22:07:38 +0200

    Refactor: Disable custom rules management in RulesTab and provide user feedback on temporary unavailability

--- src/types/api/rule/CreateRuleRequest.d.ts
+++
@@ -1,8 +0,0 @@
-interface CreateRuleRequest {
-  name: string;
-  type: RuleType;
-  scope_type: RuleScopeType;
-  scope_id: number | null;
-  params: AnyRuleParams;
-  enabled: boolean;
-}
--- src/types/api/rule/UpdateRuleRequest.d.ts
+++
@@ -1,1 +0,0 @@
-type UpdateRuleRequest = Partial<CreateRuleRequest>;
